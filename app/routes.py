ffcde09a331d0a8fdeb93f3564fa5ed340cb243b
kbieler <kristenbieler@gmail.com>
2022-05-19 15:14:00 -0500

    third commit

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,27 @@
 #we need access to our Flask object
 from app import app
 
+from flask import render_template 
+
+import requests as r
+
 #route deocrator will go by the following syntax:
 # @<flask object/blueprint name>.route('/url endpoint', <methods>)
 
 @app.route('/')
 def home():
-    greeting = 'Hello, Foxes!'
+    greeting = 'Welcome to Flask week, Foxes!'
     print(greeting)
-    return 'Hello, world.'
+    return render_template('index.html', g=greeting) 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/players')
+def players():
+    #make an API call and utilize info from that PI call in teh HTML templating
+    data = r.get('https://foxes90-prempundit.herokuapp.com/players')
+    if data.status_code == 200:
+        data = data.json()
+    return render_template('players.html', data = data)
